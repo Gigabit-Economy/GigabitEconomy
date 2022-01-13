b251344d123581c47990a3b2424bb854264c52bb
Tom Coward <34864926+tom-coward@users.noreply.github.com>
2022-01-13 00:20:16 +0000

    set min window width to 1920

--- desktop/src/com/mygdx/gigabiteconomy/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/gigabiteconomy/desktop/DesktopLauncher.java
@@ -8,7 +8,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setTitle("Gigabit Economy");
-		config.setWindowSizeLimits(-1, -1, 1920, 1080);
+		config.setWindowSizeLimits(1920, -1, 1920, 1080);
 		System.out.println("Creating new GBit");
 		new Lwjgl3Application(new GigabitEconomy(), config);
 	}
