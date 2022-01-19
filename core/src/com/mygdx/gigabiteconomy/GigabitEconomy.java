72a53b9c322109d226e800d3c80127fabd9bbe43
egd0r <e.veigarianne@lancaster.ac.uk>
2022-01-19 17:21:45 +0000

    Created hashmap for screens & director methods to preserve encapsulation

--- core/src/com/mygdx/gigabiteconomy/GigabitEconomy.java
+++ core/src/com/mygdx/gigabiteconomy/GigabitEconomy.java
@@ -1,20 +1,38 @@
 package com.mygdx.gigabiteconomy;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.renderers.OrthoCachedTiledMapRenderer;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.utils.viewport.FillViewport;
 import com.mygdx.gigabiteconomy.scenes.MainScreen;
+import com.mygdx.gigabiteconomy.scenes.MenuScreen;
+import jdk.tools.jmod.Main;
+import org.graalvm.compiler.core.common.type.ArithmeticOpTable;
 
 import java.util.HashMap;
 
 public class GigabitEconomy extends Game {
 
+    OrthographicCamera camera;
+    FillViewport vp;
+
+    HashMap<String, Screen> screens = new HashMap<>();
 
     @Override
     public void create() {
+        camera = new OrthographicCamera(1920, 1080);
+        vp = new FillViewport(5760, 1080, camera);
+
+
         System.out.println("Creating screen");
-        setScreen(new MainScreen());
+        //Change to MenuScreen()
+        //Passing director (this class) to menu screen so we can call swap screen method
+        screens.put("menu", new MenuScreen(this));
+        screens.put("main", new MainScreen(this));
+
+        setScreen(screens.get("main"));
     }
 
     @Override
@@ -41,4 +59,14 @@ public class GigabitEconomy extends Game {
 
     }
 
+    public void updateCameraPos(int x, int y) {
+        camera.position.set(x, y, 0);
+        camera.update();
+
+    }
+
+    public Matrix4 getCombined() {
+        //So batch can use camera coords
+        return camera.combined;
+    }
 }
---
+++ core/src/com/mygdx/gigabiteconomy/scenes/MenuScreen.java
@@ -0,0 +1,50 @@
+package com.mygdx.gigabiteconomy.scenes;
+
+
+import com.badlogic.gdx.Screen;
+import com.mygdx.gigabiteconomy.GigabitEconomy;
+
+public class MenuScreen implements Screen {
+
+    GigabitEconomy director;
+
+    public MenuScreen(GigabitEconomy director) {
+        this.director = director;
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
