e7d2dc0f7aa6a288eb9c67a91f11236fdba484a2
Tom Coward <34864926+tom-coward@users.noreply.github.com>
2022-01-26 20:57:50 +0000

    Add JavaDoc comments to LevelScreen & LevelOneScreen

--- core/src/com/mygdx/gigabiteconomy/screens/LevelOneScreen.java
+++ core/src/com/mygdx/gigabiteconomy/screens/LevelOneScreen.java
@@ -13,11 +13,21 @@ import java.util.ArrayList;
  * Level 1 screen
  */
 public class LevelOneScreen extends LevelScreen {
+    // Player character
     private static final Player PLAYER = new Player("amzn_9iron.txt",0 , 0);
+    // Enemy 1 character
     private static final Enemy ENEMY_ONE = new Enemy("amzn_9iron.txt", 500, 500);
+    // ArrayList of all enemy characters
     private static final ArrayList<GameObject> ENEMIES = new ArrayList<GameObject>(Arrays.asList(ENEMY_ONE));
+    // Level screen background texture
     private static final Texture BACKGROUND_TEXTURE = new Texture("finished_assets/levels/level1.png");
 
+    /**
+     * Creates a new screen instance for Level 1 based off the LevelScreen abstract class, which contains all shared
+     * methods/properties between levels and implements the LibGDX Screen interface.
+     *
+     * @param director the instance of the game director
+     */
     public LevelOneScreen(GigabitEconomy director) {
         super(director, PLAYER, ENEMIES, BACKGROUND_TEXTURE);
     }
--- core/src/com/mygdx/gigabiteconomy/screens/LevelScreen.java
+++ core/src/com/mygdx/gigabiteconomy/screens/LevelScreen.java
@@ -27,6 +27,15 @@ abstract class LevelScreen implements Screen, ApplicationListener {
     private Player player;
     private ArrayList<GameObject> enemies;
 
+    /**
+     * A template constructor for use by all level screen subclasses. Sets properties that differ between levels
+     * such as game director, player/enemy character sprites & background texture.
+     *
+     * @param director the instance of the game director
+     * @param player the player character for the level
+     * @param enemies an ArrayList containing all enemy characters for the level
+     * @param backgroundTexture the background graphic of the level
+     */
     public LevelScreen(GigabitEconomy director, Player player, ArrayList<GameObject> enemies, Texture backgroundTexture) {
         this.director = director;
         this.player = player;
@@ -34,6 +43,11 @@ abstract class LevelScreen implements Screen, ApplicationListener {
         this.backgroundTexture = backgroundTexture;
     }
 
+    /**
+     * Initialises the sprite batch (which contains all character sprites) and adds the player/enemy characters to it.
+     * Called by LibGDX when the screen is set as active by the game director.
+     */
+    @Override
     public void show() {
         batch = new SpriteBatch();
 
@@ -48,6 +62,14 @@ abstract class LevelScreen implements Screen, ApplicationListener {
         sprites.addAll(enemies);
     }
 
+    /**
+     * Sets the current view of the screen, drawing sprites on the screen based on their current position; also
+     * calls the player collision check system to check for any collisions since the previous render.
+     * Called by LibGDX upon each render of the screen.
+     *
+     * @param delta the time elapsed since the previous render (in seconds)
+     */
+    @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
@@ -75,6 +97,10 @@ abstract class LevelScreen implements Screen, ApplicationListener {
         playerCollisionCheck();
     }
 
+    /**
+     * Iterates through each enemy character sprite, checking if the player character sprite is overlapping
+     * and has therefore collided with the enemy.
+     */
     private void playerCollisionCheck() {
         for (GameObject enemy : enemies) {
             if (player.getRectangle().overlaps(enemy.getRectangle())) {
@@ -105,11 +131,22 @@ abstract class LevelScreen implements Screen, ApplicationListener {
     public void resume() {
     }
 
+    /**
+     * Sets the input processor to null to prevent the LevelScreen's application listener from listening to user
+     * inputs; then calls dispose() to remove screen assets from memory.
+     * Called by LibGDX when the screen is made inactive.
+     */
     @Override
     public void hide() {
         Gdx.input.setInputProcessor(null);
+        dispose();
     }
 
+    /**
+     * Removes the screens assets (background texture and sprite batch) from memory when the screen is made inactive
+     * and they're therefore no longer needed.
+     * Called by hide().
+     */
     @Override
     public void dispose() {
         backgroundTexture.dispose();
