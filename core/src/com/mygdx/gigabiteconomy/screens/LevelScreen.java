550500b5111e84e347afad688b0481c9a8060eb6
Tom Coward <34864926+tom-coward@users.noreply.github.com>
2022-02-22 11:32:52 +0000

    set level of player when added to LevelScreen

--- core/src/com/mygdx/gigabiteconomy/screens/LevelScreen.java
+++ core/src/com/mygdx/gigabiteconomy/screens/LevelScreen.java
@@ -71,6 +71,7 @@ public abstract class LevelScreen implements Screen, InputProcessor {
      */
     public void addPlayer(Player player) {
         this.player = player;
+        player.setLevel(this);
 
         addSprite(player);
     }
