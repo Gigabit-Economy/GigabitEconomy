885e26e562a2e7a198991bdfbc5a31fb033c6448
Edgar <e.veigarianne@lancaster.ac.uk>
2022-02-24 09:13:55 +0000

    Health bar displays in correct position

--- core/src/com/mygdx/gigabiteconomy/sprites/tiled/Player.java
+++ core/src/com/mygdx/gigabiteconomy/sprites/tiled/Player.java
@@ -268,9 +268,9 @@ public class Player extends MovingSprite {
         private int[] dimensions; // [ H , W ]
 
         public HealthBar() {
-            super(300, 1000);
+            super(50, 900);
             healthRect = new ShapeRenderer();
-            dimensions = new int[]{100, 30}; // More specific values needed, size of health bar texture
+            dimensions = new int[]{318, 72}; // More specific values needed, size of health bar texture
             healthBarTexture = new Texture("finished_assets/ui_elements/health bar1.png");
         }
 
@@ -280,15 +280,15 @@ public class Player extends MovingSprite {
         }
 
         public void drawOn(SpriteBatch batch) {
-            batch.draw(healthBarTexture, getX(), getY());
             batch.end();
 
             healthRect.begin(ShapeRenderer.ShapeType.Filled);
             healthRect.setColor(Color.RED);
-            healthRect.rect(getX()+10, getY(), dimensions[0], dimensions[1]);
+            healthRect.rect(getX()+100, getY()+39, dimensions[0], dimensions[1]);
             healthRect.end();
 
             batch.begin();
+            batch.draw(healthBarTexture, getX(), getY());
         }
 
         public void modifyHeath(int dHealth) {
