7c3ed9b06b743bac7467ca2922a0e29919409cd2
Tom Coward <34864926+tom-coward@users.noreply.github.com>
2022-03-01 22:51:14 +0000

    update Enemy constructor javadoc

--- core/src/com/mygdx/gigabiteconomy/sprites/tiled/Enemy.java
+++ core/src/com/mygdx/gigabiteconomy/sprites/tiled/Enemy.java
@@ -45,7 +45,7 @@ public abstract class Enemy extends MovingSprite {
     /**
      * Create a new Enemy sprite (MovingSprite)
      *
-     * @param BASE_PATH of texture
+     * @param level the level of sprite (dictates the path of the texture)
      * @param weapon the weapon the Enemy is carrying
      * @param x position of Tile (within tile grid) to place sprite
      * @param y position of Tile (within tile grid) to place sprite
